03da389e91ec7ab624d415aebd4c096337261bbf
AlexTalko <talko.alexander@gmail.com>
2024-02-04 22:04:26 +0400

    добавили файл str_func.py добавили в него функцию, котороая возвращает введенную строку заглавными буквами

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
 def upper_str():
     s = input().upper()
     return s
+
+
+def title_str():
+    """
+    функция вернет введенную строку словами с заглавными буквами
+    """
+    s = input().title()
+    return s
